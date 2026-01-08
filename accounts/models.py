77d00c13657f7e31edc932fc43e6466389b1eec4
Upasana Dhameliya (Ana) <upasana.dhameliya@gmail.com>
2026-01-08 16:50:30 +0530

    feat: creat custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('reader', 'Reader'),
+        ('author', 'Author'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='reader'
+    )
+    bio = models.TextField(blank=True)
+    profile_image = models.ImageField(
+        upload_to='profiles/',
+        blank=True,
+        null=True
+    )
+
+    def is_author(self):
+        return self.role == 'author'
